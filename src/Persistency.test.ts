cbe0e6dc9b744edc1400b3a34c9a57e181ae8f66
Llorx <dallorx@gmail.com>
2025-05-11 12:10:25 +0200

    test: Add memory blocks and truncate tests

--- src/Persistency.test.ts
+++ src/Persistency.test.ts
@@ -11,19 +11,29 @@ import { sha256 } from "./utils";
 
 test.describe("Persistency", test => {
     // helpers
+    const entrySize = constants.EntryHeaderOffsets_V0.SIZE + constants.EntryOffsets_V0.SIZE;
     const value1 = Buffer.from([ 0, 1, 2, 3,  4,  5 ]);
     const value2 = Buffer.from([ 6, 7, 8, 9, 10, 11 ]);
     const value3 = Buffer.from([ 0, 2, 4, 6,  8, 10 ]);
     const value4 = Buffer.from([ 1, 3, 5, 7,  9, 11 ]);
-    async function setValueTs(persistency:Pick<Persistency, "entriesFile">, valueI:number, ts:number) {
-        const buffer = await Fs.readFile(persistency.entriesFile);
-        const entryLocation = constants.MAGIC.length + ((constants.EntryHeaderOffsets_V0.SIZE + constants.EntryOffsets_V0.SIZE) * valueI);
-        buffer.writeUint32BE(ts, entryLocation + constants.EntryHeaderOffsets_V0.SIZE + constants.EntryOffsets_V0.TS);
-        await hashEntry(persistency, valueI, buffer);
+    function getEntryOffset(entryI:number) {
+        return constants.MAGIC.length + (entrySize * entryI);
     }
-    async function hashEntry(persistency:Pick<Persistency, "entriesFile">, valueI:number, buffer?:Buffer) {
+    function getDataOffset(keyLength:number, dataI:number) {
+        // All values have same length (value1, value2, etc)
+        return {
+            start: constants.MAGIC.length + ((keyLength + value1.length) * dataI),
+            end: constants.MAGIC.length + ((keyLength + value1.length) * (dataI + 1))
+        };
+    }
+    async function setValueTs(persistency:Pick<Persistency, "entriesFile">, entryI:number, ts:number) {
+        const buffer = await Fs.readFile(persistency.entriesFile);
+        buffer.writeUint32BE(ts, getEntryOffset(entryI) + constants.EntryHeaderOffsets_V0.SIZE + constants.EntryOffsets_V0.TS);
+        await hashEntry(persistency, entryI, buffer);
+    }
+    async function hashEntry(persistency:Pick<Persistency, "entriesFile">, entryI:number, buffer?:Buffer) {
         buffer = buffer || await Fs.readFile(persistency.entriesFile);
-        const entryLocation = constants.MAGIC.length + ((constants.EntryHeaderOffsets_V0.SIZE + constants.EntryOffsets_V0.SIZE) * valueI);
+        const entryLocation = getEntryOffset(entryI);
         const entryDataLocation = entryLocation + constants.EntryHeaderOffsets_V0.SIZE;
         const hash = sha256(buffer.subarray(entryDataLocation, entryDataLocation + constants.EntryOffsets_V0.SIZE));
         hash.copy(buffer, entryLocation + constants.EntryHeaderOffsets_V0.ENTRY_HASH);
@@ -36,6 +46,11 @@ test.describe("Persistency", test => {
             reclaimTimeout: options?.reclaimTimeout
         }, mock), persistency => persistency.close());
         return { persistency, folder };
+    }
+    async function setEntryBytes(persistency:Pick<Persistency, "entriesFile">, entryI:number, offset:number, bytes:Buffer) {
+        const buffer = await Fs.readFile(persistency.entriesFile);
+        bytes.copy(buffer, getEntryOffset(entryI) + offset);
+        await Fs.writeFile(persistency.entriesFile, buffer);
     }
     async function getFileSize(file:string) {
         return (await Fs.stat(file)).size;
@@ -61,7 +76,7 @@ test.describe("Persistency", test => {
         };
     }
     // end helpers
-    test("Should set and get data", {
+    test("should set and get data", {
         ARRANGE(after) {
             return newPersistency(after);
         },
@@ -72,7 +87,7 @@ test.describe("Persistency", test => {
             Assert.deepStrictEqual(persistency.get("test"), value1);
         }
     });
-    test("Should load data from file", {
+    test("should load data from file", {
         async ARRANGE(after) {
             const { persistency, folder } = await newPersistency(after);
             persistency.set("test", value1);
@@ -92,7 +107,7 @@ test.describe("Persistency", test => {
             }
         }
     });
-    test("Should update data", {
+    test("should update data", {
         async ARRANGE(after) {
             const { persistency, folder } = await newPersistency(after);
             persistency.set("test", value1);
@@ -107,7 +122,7 @@ test.describe("Persistency", test => {
             Assert.deepStrictEqual(persistency.get("test"), value2);
         }
     });
-    test("Should update data without reloading", {
+    test("should update data without reloading", {
         ARRANGE(after) {
             return newPersistency(after);
         },
@@ -119,7 +134,7 @@ test.describe("Persistency", test => {
             Assert.deepStrictEqual(persistency.get("test"), value2);
         }
     });
-    test("Should delete data", {
+    test("should delete data", {
         async ARRANGE(after) {
             const { persistency } = await newPersistency(after);
             persistency.set("test", value1);
@@ -138,7 +153,7 @@ test.describe("Persistency", test => {
             }
         }
     });
-    test("Should not load previous deleted data from file", {
+    test("should not load previous deleted data from file", {
         async ARRANGE(after) {
             const { persistency, folder: folder } = await newPersistency(after);
             persistency.set("test", value1);
@@ -159,7 +174,7 @@ test.describe("Persistency", test => {
             }
         }
     });
-    test("Should not load deleted entry with multiple subentries from file", {
+    test("should not load deleted entry with multiple subentries from file", {
         async ARRANGE(after) {
             const { persistency, folder: folder } = await newPersistency(after);
             persistency.set("test", value1);
@@ -175,7 +190,7 @@ test.describe("Persistency", test => {
             Assert.strictEqual(persistency.get("test"), null);
         }
     });
-    test("Should load wrapped around entry when second value is bigger", {
+    test("should load wrapped around entry when second value is bigger", {
         async ARRANGE(after) {
             const { persistency, folder: folder } = await newPersistency(after);
             persistency.set("test", value1);
@@ -191,7 +206,7 @@ test.describe("Persistency", test => {
             Assert.deepStrictEqual(persistency.get("test"), value1);
         }
     });
-    test("Should load wrapped around entry when first value is bigger", {
+    test("should load wrapped around entry when first value is bigger", {
         async ARRANGE(after) {
             const { persistency, folder: folder } = await newPersistency(after);
             persistency.set("test", value1);
@@ -207,7 +222,7 @@ test.describe("Persistency", test => {
             Assert.deepStrictEqual(persistency.get("test"), value2);
         }
     });
-    test("Should overwrite deleted entries", {
+    test("should overwrite deleted entries", {
         async ARRANGE(after) {
             const { persistency } = await newPersistency(after);
             persistency.set("aaa", value1);
@@ -227,7 +242,7 @@ test.describe("Persistency", test => {
             }
         }
     });
-    test("Should purge old entry after time", {
+    test("should purge old entry after time", {
         async ARRANGE(after) {
             const context = newContextMock();
             const { persistency } = await newPersistency(after, {
@@ -256,7 +271,7 @@ test.describe("Persistency", test => {
             }
         }
     });
-    test("Should clean purge array when deleting a purging entry", {
+    test("should clean purge array when deleting a purging entry", {
         async ARRANGE(after) {
             const context = newContextMock();
             const { persistency } = await newPersistency(after, {
@@ -277,7 +292,7 @@ test.describe("Persistency", test => {
             Assert.deepStrictEqual(persistency.get("bbb"), value4);
         }
     });
-    test("Should overwrite all deleted bytes from the same entry with multiple entries", {
+    test("should overwrite all deleted bytes from the same entry with multiple entries", {
         async ARRANGE(after) {
             const { persistency } = await newPersistency(after);
             persistency.set("aaa", value1);
@@ -300,15 +315,105 @@ test.describe("Persistency", test => {
             }
         }
     });
+    test("should load allocated blocks on file load", {
+        async ARRANGE(after) {
+            const { persistency, folder } = await newPersistency(after);
+            // all keys same length for easy offset calculation purposes
+            persistency.set("test0", value1);
+            persistency.set("test1", value2);
+            persistency.set("test2", value3);
+            persistency.set("test3", value4);
+            persistency.set("test4", value1);
+            persistency.set("test5", value2);
+            persistency.set("test6", value3);
+            persistency.set("test7", value4);
+            persistency.delete("test2");
+            persistency.delete("test1");
+            persistency.delete("test7");
+            persistency.delete("test4");
+            persistency.close();
+            return { folder };
+        },
+        ACT({ folder }, after) {
+            return newPersistency(after, { folder });
+        },
+        ASSERTS: {
+            "should have allocated entries in memory"({ persistency }) {
+                Assert.deepStrictEqual(persistency.getAllocatedBlocks().entries, [
+                    [0, getEntryOffset(0) + entrySize],
+                    [getEntryOffset(3), getEntryOffset(3) + entrySize],
+                    [getEntryOffset(5), getEntryOffset(6) + entrySize]
+                ]);
+            },
+            "should have allocated data in memory"({ persistency }) {
+                Assert.deepStrictEqual(persistency.getAllocatedBlocks().data, [
+                    [0, getDataOffset(5, 0).end], // keylength 5
+                    [getDataOffset(5, 3).start, getDataOffset(5, 3).end], // keylength 5
+                    [getDataOffset(5, 5).start, getDataOffset(5, 6).end] // keylength 5
+                ]);
+            }
+        }
+    });
+    test.describe("truncate", test => {
+        test("should truncate files if freeing a final entry", {
+            async ARRANGE(after) {
+                const { persistency } = await newPersistency(after);
+                persistency.set("test0", value1);
+                persistency.set("test1", value2);
+                persistency.set("test2", value3);
+                persistency.set("test3", value4);
+                const fileSizes = await getFileSizes(persistency);
+                return { persistency, fileSizes };
+            },
+            ACT({ persistency }) {
+                persistency.delete("test3");
+            },
+            ASSERTS: {
+                "should not have entry3"(_, { persistency }) {
+                    Assert.strictEqual(persistency.get("entry3"), null);
+                },
+                async "should truncate entries file"(_, { persistency, fileSizes }) {
+                    Assert.strictEqual(await getFileSize(persistency.entriesFile) < fileSizes.entries, true);
+                },
+                async "should truncate data file"(_, { persistency, fileSizes }) {
+                    Assert.strictEqual(await getFileSize(persistency.dataFile) < fileSizes.data, true);
+                }
+            }
+        });
+        test("should truncate files on load", {
+            async ARRANGE(after) {
+                const { persistency, folder } = await newPersistency(after);
+                persistency.set("test0", value1);
+                persistency.set("test1", value2);
+                persistency.set("test2", value3);
+                persistency.set("test3", value4);
+                persistency.close();
+                const fileSizes = await getFileSizes(persistency);
+                await setEntryBytes(persistency, 3, 3, Buffer.from([ 0x00, 0x01, 0xFF ])); // Invalidate entry3
+                return { folder, fileSizes };
+            },
+            ACT({ folder }, after) {
+                return newPersistency(after, { folder });
+            },
+            ASSERTS: {
+                "should not have entry3"({ persistency }) {
+                    Assert.strictEqual(persistency.get("entry3"), null);
+                },
+                async "should truncate entries file"({ persistency }, { fileSizes }) {
+                    Assert.strictEqual(await getFileSize(persistency.entriesFile) < fileSizes.entries, true);
+                },
+                async "should truncate data file"({ persistency }, { fileSizes }) {
+                    Assert.strictEqual(await getFileSize(persistency.dataFile) < fileSizes.data, true);
+                }
+            }
+        });
+    });
     // TODO:
     // Testear que count() contiene la cantidad de entradas correspondientes
     // Testear que cuando se purga un dato, desaparece la entry correspondiente del array de entries
     // Testear que el dato desaparece si tooodas las entradas han sido purgadas (no tengo claro cómo puede ocurrir esto)
 
-    // Testear que al inicio genera los huecos tanto de entries como datas correspondientes.
-    // Cómo testear? Pues devolviendo el json de huecos directamente y yaw...
-
     // Y ya testear todas las posibilidades al cargar (incluyendo que trunca)
-    // Testear que trunca si hace free de un dato al final
-    // Testear cuando se escriben datos parciales de absolutamente todos los bytes posibles
+    // Testear cuando se escriben datos parciales de absolutamente todos los bytes posibles (entrada y datos)
+    // Testear esto además cuando se van a borrar entradas (bytes parciales escritos)
 });
